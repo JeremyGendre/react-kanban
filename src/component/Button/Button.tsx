d5d2a11cd7430916ae2478ed3d67c66bb6cf6ea0
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-06-19 21:01:38 +0200

    style: kanban col

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -2,12 +2,14 @@ import {PropsWithChildren} from "react";
 import './Button.css';
 
 interface ButtonProps extends Partial<JSX.IntrinsicElements['button']>{
-    small?: boolean
+    small?: boolean;
+    uncolored?: boolean;
+    iconButton?: boolean;
 }
 
-export default function Button({children, small, className, ...other}:PropsWithChildren<ButtonProps>){
+export default function Button({children, small, uncolored, iconButton, className, ...other}:PropsWithChildren<ButtonProps>){
     return (
-        <button className={`button ${small ? 'button-small' : ''} ${className}`} {...other}>
+        <button className={`button ${small ? 'button-small' : ''} ${uncolored ? 'button-uncolored' : ''} ${iconButton ? 'button-icon' : ''} ${className}`} {...other}>
             {children}
         </button>
     );
--- src/component/KanbanCol/KanbanCol.tsx
+++ src/component/KanbanCol/KanbanCol.tsx
@@ -1,5 +1,7 @@
 import './KanbanCol.css';
 import {PropsWithChildren} from "react";
+import DeleteIcon from "../icons/DeleteIcon";
+import Button from "../Button/Button";
 
 
 interface KanbanColProps {
@@ -9,10 +11,14 @@ interface KanbanColProps {
 export default function KanbanCol({children, title}: PropsWithChildren<KanbanColProps>){
     return (
         <div className="kanban-col">
-            <div className="kanban-col-title">{title}</div>
+            <div className="kanban-col-title">
+                <div className="my-auto">{title}</div>
+                <Button uncolored small iconButton><DeleteIcon className="my-auto" style={{width:'1.2em'}}/></Button>
+            </div>
             <div className="kanban-col-item-container">
                 {children}
             </div>
+            <div className="kanban-new-item-form">new item</div>
         </div>
     );
 }
