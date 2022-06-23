1bea22113bdd60b7dd3b830fb3471815d3232706
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-06-23 10:10:42 +0200

    feat: update col name

--- src/component/KanbanCol/KanbanCol.tsx
+++ src/component/KanbanCol/KanbanCol.tsx
@@ -1,10 +1,13 @@
 import './KanbanCol.css';
-import {PropsWithChildren, useState} from "react";
+import {FormEvent, PropsWithChildren, useState} from "react";
 import DeleteIcon from "../icons/DeleteIcon";
 import Button from "../Button/Button";
 import PlusIcon from "../icons/PlusIcon";
 import NewItemForm from "../KanbanItem/NewItemForm";
 import {useDraggable} from "../../context/DraggableContext";
+import CheckIcon from "../icons/CheckIcon";
+import Input from "../Input/Input";
+import {useKanban} from "../../context/KanbanContext";
 
 interface KanbanColProps {
     title: string;
@@ -16,17 +19,50 @@ interface KanbanColProps {
 
 export default function KanbanCol({children,deletable, id, title, onDelete, onNewItem}: PropsWithChildren<KanbanColProps>){
     const [showForm, setShowForm] = useState(false);
+    const [showEditForm, setShowEditForm] = useState(false);
+    const [newTitle, setNewTitle] = useState('');
     const {drop} = useDraggable();
+    const {setKanban} = useKanban();
+
+    const handleSubmit = (e:FormEvent) => {
+        e.preventDefault();
+        if(!newTitle) return;
+        setKanban(prev => {
+            prev[id] = {...prev[id], title: newTitle};
+            return [...prev];
+        });
+        setShowEditForm(false);
+    };
 
     return (
         <div className="kanban-col">
             <div className="kanban-col-title">
-                <div className="my-auto flex-1">{title}</div>
-                {deletable && (
-                    <Button title="Delete this column" uncolored small iconButton onClick={onDelete}>
-                        <DeleteIcon className="my-auto" style={{width:'1.2em'}}/>
-                    </Button>
-                )}
+                <div className="my-auto flex-1">
+                    {showEditForm ? (
+                        <form onSubmit={handleSubmit}>
+                            <div className="w-full d-flex">
+                                <Input required autoFocus className="flex-1" value={newTitle} onChange={e => setNewTitle(e.currentTarget.value)}/>
+                            </div>
+                            <div className="d-flex">
+                                <Button type="submit" title="Enregistrer" uncolored small iconButton>
+                                    <CheckIcon className="my-auto" style={{width:'1.2em'}}/>
+                                </Button>
+                                <Button type="button" title="Enregistrer" uncolored small iconButton onClick={() => {setNewTitle(''); setShowEditForm(false);}}>
+                                    <DeleteIcon className="my-auto" style={{width:'1.2em'}}/>
+                                </Button>
+                            </div>
+                        </form>
+                    ) : (
+                        <div className="d-flex">
+                            <div className="flex-1 my-auto cursor-pointer" onClick={() => setShowEditForm(true)}>{title}</div>
+                            {deletable && (
+                                <Button title="Delete this column" uncolored small iconButton onClick={onDelete}>
+                                    <DeleteIcon className="my-auto" style={{width:'1.2em'}}/>
+                                </Button>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
             <div
                 className="kanban-col-item-container"
@@ -53,3 +89,4 @@ export default function KanbanCol({children,deletable, id, title, onDelete, onNe
         </div>
     );
 }
+
